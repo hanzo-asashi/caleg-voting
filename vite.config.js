c938be22a3b15e90c4a18d8b6b2e6267556b869f
Hansen Makangiras <hansenmakangiras@gmail.com>
2023-04-11 16:18:03 +0800

    [change] login with username or email.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import fs from "fs";
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
+import { homedir } from "os";
+import { resolve } from "path";
+
+let host = "caleg-voting.local";
 
 export default defineConfig({
     plugins: [
@@ -11,4 +16,34 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+
+    server: detectServerConfig(host),
 });
+
+function detectServerConfig(host) {
+    let keyPath = resolve(
+        homedir(),
+        "D:/Development/laragon/etc/ssl/laragon.key"
+    );
+    let certificatePath = resolve(
+        homedir(),
+        "D:/Development/laragon/etc/ssl/laragon.crt"
+    );
+
+    if (!fs.existsSync(keyPath)) {
+        return {};
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {};
+    }
+
+    return {
+        hmr: { host },
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    };
+}
